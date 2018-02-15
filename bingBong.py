3415321a1ed6d8e13e111ee3c1cdac4efe023410
cwylie0 <cwylie0@hotmail.com>
2018-02-15 14:32:37 -0500

    Removed old version.

--- bingBong.py
+++ bingBong.py
@@ -1,10 +1,26 @@
 import webbrowser
 import time
+import random
 
 url = "http://www.bing.com/search?q="
 imageUrl = "http://www.bing.com/images/search?q="
-nb = raw_input('Enter search terms: ')
-for i, word in enumerate(nb.split()):
-	print "Word #%d: %s" % (i, word)
-	webbrowser.open_new_tab(imageUrl+word)
-	time.sleep(5)
+nb = raw_input('Enter filename: ')
+counter = raw_input('Enter points needed: ')
+counting = int(counter) * 2
+file = open(nb, 'r')
+
+wordList = []
+with open(nb,'r') as f:
+    for line in f:
+        for word in line.split():
+            wordList.append(word)
+	     
+i = 1
+while i <= counting:
+        queryTerm = wordList[random.randrange(0, len(wordList))]
+	print "Word #%d: %s" % (i, queryTerm)
+	webbrowser.open_new_tab(imageUrl+queryTerm)
+	time.sleep(3)
+	i += 1
+
+file.close()
