7280bbff4b86ee052de6f986c34fa20c7a0bfbf9
cwylie0 <cwylie0@hotmail.com>
2018-09-26 17:55:24 -0400

    updated bingBong.py

--- bingBong.py
+++ bingBong.py
@@ -1,12 +1,16 @@
 import webbrowser
 import time
 import random
+import string
+
 
 url = "http://www.bing.com/search?q="
 imageUrl = "http://www.bing.com/images/search?q="
-nb = raw_input('Enter filename: ')
-counter = raw_input('Enter points needed: ')
-counting = int(counter) * 2
+exclude = set(string.punctuation)
+
+nb = input('Enter filename: ')
+counter = input('Enter points needed: ')
+counting = (int(counter)  / 10) * 2
 file = open(nb, 'r')
 
 wordList = []
@@ -18,9 +22,14 @@ with open(nb,'r') as f:
 i = 1
 while i <= counting:
         queryTerm = wordList[random.randrange(0, len(wordList))]
-	print "Word #%d: %s" % (i, queryTerm)
-	webbrowser.open_new_tab(imageUrl+queryTerm)
-	time.sleep(3)
-	i += 1
+        while len(queryTerm) < 5:
+            queryTerm = wordList[random.randrange(0, len(wordList))]
+        
+        for c in string.punctuation:
+            queryTerm=queryTerm.replace(c,"")
+        print ("Word #%d: %s" % (i, queryTerm))
+        webbrowser.open_new_tab(imageUrl+queryTerm)
+        time.sleep(1)
+        i += 1
 
 file.close()
